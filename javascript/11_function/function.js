d60914e72c8edfc5f0e7b517f184b607f6167b90
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-07 10:29:07 +0530

    we write program related from this context

--- javascript/11_function/function.js
+++ javascript/11_function/function.js
@@ -35,3 +35,53 @@ function sum(...numbers) {
 }
 console.log(sum(1, 2, 3, 4, 5)); // 15
 
+// now we will see more case about this context.
+const obj = {
+  value: 42,
+  getValue: function () {
+    return this.value;
+  },
+};
+console.log(obj.getValue()); // 42
+
+const obj2 = {
+  value: 42,
+  getValue: () => {
+    return this.value;
+  },
+};
+console.log(obj2.getValue()); // undefined
+
+// function constructor
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+const person = new Person("John", 30);
+console.log(person.name); // John
+
+// this context when function present in array in object.
+const obj3 = {
+  values: [1, 2, 3],
+  getValues: function () {
+    return this.values.map((value) => value * 2);
+  },
+};
+console.log(obj3.getValues()); // [2, 4, 6]
+
+//output
+
+/**
+5
+6
+-1
+0.6666666666666666
+Hello, Guest!
+Hello, John!
+15
+42
+undefined
+John
+[ 2, 4, 6 ]
+*/
