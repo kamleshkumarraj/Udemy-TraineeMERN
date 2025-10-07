f12767808214fc51bcbb5bd503d85480b3c309f2
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-07 10:04:18 +0530

    we write the output for this program

--- javascript/09_loop/index.js
+++ javascript/09_loop/index.js
@@ -43,3 +43,54 @@ if (isPrime) {
 } else {
   console.log(`${number} is not a prime number`);
 }
+
+// output for this code.
+/*
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+While loop ended
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Do while loop ended
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+For loop ended
+5 * 1 = 5
+5 * 2 = 10
+5 * 3 = 15
+5 * 4 = 20
+5 * 5 = 25
+5 * 6 = 30
+5 * 7 = 35
+5 * 8 = 40
+5 * 9 = 45
+5 * 10 = 50
+13 is a prime number
+*/
