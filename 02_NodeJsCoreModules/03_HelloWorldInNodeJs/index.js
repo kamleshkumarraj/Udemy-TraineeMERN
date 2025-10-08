8c68c181457697d4df39e87f4acc63533e7b6c3a
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-08 13:25:38 +0530

    we run simple program in node js

---
+++ 02_NodeJsCoreModules/03_HelloWorldInNodeJs/index.js
@@ -0,0 +1,3 @@
+// A simple Node.js program to print "Hello, World!" to the console
+console.log("Hello, World! This is my first Node.js program.");
+// Output: Hello, World! This is my first Node.js program.
