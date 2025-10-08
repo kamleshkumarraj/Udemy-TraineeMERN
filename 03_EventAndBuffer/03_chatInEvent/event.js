fd05a9709c4b12b9755fdbe4f985063f9b2848cf
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-08 15:09:43 +0530

    we create class for handle all event

---
+++ 03_EventAndBuffer/03_chatInEvent/event.js
@@ -0,0 +1,34 @@
+const EventEmitter  = require("events");
+
+class ChatApp extends EventEmitter{
+  constructor(){
+    super();
+    this.users = new Map();
+  }
+  login(username){
+    if( this.users.has(username) ){
+      console.log(`${username} is already logged in`);
+      return;
+    }
+    this.users.set(username, true);
+    this.currentUser = username;
+    this.emit("login", username);
+  }
+
+  logout(username){
+    if( !this.users.has(username) ){
+      console.log(`${username} is not logged in`);
+      return;
+    }
+    this.users.delete(username);
+    this.emit("logout", username);
+  }
+
+  sendMessage(username, message){
+    if( !this.users.has(username) ){
+      console.log(`${username} is not logged in. Please login to send messages.`);
+      return;
+    }
+    this.emit("message", username, message);
+  }
+}
