63c19a894a4f8814749dfa53fea48a42a26bff4f
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-09 15:10:37 +0530

    we write code for register the user in our application

---
+++ 06_BookStoreProject/server/src/controllers/auth.js
@@ -0,0 +1,33 @@
+// first we write code for register the user on our website.
+
+import { asyncErrorHandler } from "../errors/asyncErrorHandler.js";
+import fs from 'fs/promises'
+import {v4 as uuid} from 'uuid'
+
+export const registerUser = asyncErrorHandler(async (req, res, next) => {
+  const {firstName, lastName, email, password, address} = req.body;
+
+  if(!firstName || !lastName || !email || !password || !address) {
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required"
+    });
+  }
+  const _id = uuid(); // unique id generate for each user.
+  const userData = {_id, firstName, lastName, email, password, address};
+
+   // first we read the existing data from the file.
+   const userDataContent = JSON.parse(await fs.readFile('../../public/data/users/users.json', 'utf-8')) || []
+
+   userDataContent.push(userData);
+
+    // now we write the data to the file.
+    await fs.writeFile('../../public/data/users/users.json', JSON.stringify(userDataContent));
+
+    res.status(200).json({
+      success: true,
+      message: "User registered successfully",
+      data: userData
+    });
+
+})
