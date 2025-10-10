3f74d55478ef1d65abb669a84ddfc0d04c2f91d1
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-10 16:41:26 +0530

    we create table for issueDetails athat holds

---
+++ 07_PostgressSQlORM/server/src/schema/issuedBooks.schema.js
@@ -0,0 +1,12 @@
+// we write schema for issued books.
+import { pgTable, uuid, integer, timestamp } from "drizzle-orm/pg-core";
+import { usersTable } from "./users.schema.js";
+
+export const issuedDetails = pgTable("issuedDetails", {
+  id: uuid().primaryKey(),
+  issuedBy: uuid().references(() => usersTable.id).notNull(),
+  issueDate: timestamp("issue_date").notNull(),
+  // we add a books columns as an array and each elem is id of a book.
+  returnData : timestamp("return_date").notNull(),
+
+});
