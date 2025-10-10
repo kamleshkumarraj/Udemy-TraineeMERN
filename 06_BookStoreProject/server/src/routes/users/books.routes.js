be86cc1c7120fc44ff32cbd074941f034edb870e
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-10 13:33:23 +0530

    we completely complete this project

--- 06_BookStoreProject/server/src/routes/users/books.routes.js
+++ 06_BookStoreProject/server/src/routes/users/books.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { issueBook, returnBook } from "../../controllers/users/books.controller.js";
+import { getAllIssuedBooks, issueBook, returnBook } from "../../controllers/users/books.controller.js";
 import { isLoggedIn } from "../../middlewares/auth.middleware.js";
 
 export const bookHandleByUserRoute = Router();
 
 bookHandleByUserRoute.route('/issue-book').post(isLoggedIn, issueBook);
 bookHandleByUserRoute.route('/return-book').delete(isLoggedIn, returnBook);
+bookHandleByUserRoute.route('/my-issued-book').get(isLoggedIn, getAllIssuedBooks)
