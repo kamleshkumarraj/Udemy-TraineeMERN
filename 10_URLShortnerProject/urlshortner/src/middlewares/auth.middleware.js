cdb366000fb89aa6f0432bb5249f80772b8dd528
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-13 18:56:49 +0530

    fix bug from auth middleware

--- 10_URLShortnerProject/urlshortner/src/middlewares/auth.middleware.js
+++ 10_URLShortnerProject/urlshortner/src/middlewares/auth.middleware.js
@@ -1,11 +1,14 @@
 // middleware for verify user is logged in or not.
+import jwt from  'jsonwebtoken'
+
+
 export const isLoggedIn = (req, res, next) => {
   const { token } = req.cookies;
   if (!token) {
     return next(new Error("You are not logged in", 401));
   }
   // now we verify token.
-  const decoded = jwt.verify(token, SECRET_KEY);
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
   if (!decoded) {
     return next(new Error("You are not logged in", 401));
   }
--- 10_URLShortnerProject/urlshortner/src/routes/auth.routes.js
+++ 10_URLShortnerProject/urlshortner/src/routes/auth.routes.js
@@ -5,10 +5,11 @@ import {
   logout,
   register,
 } from "../controllers/auth/auth.controller.js";
+import { isLoggedIn } from "../middlewares/auth.middleware.js";
 
 export const authRouter = Router();
 
 authRouter.route("/login").post(login);
-authRouter.route("/logout").post(logout);
+authRouter.route("/logout").post(isLoggedIn, logout);
 authRouter.route("/register").post(register);
-authRouter.route('/profile').get(getProfile)
+authRouter.route('/profile').get(isLoggedIn,getProfile)
