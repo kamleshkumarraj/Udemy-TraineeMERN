b5a7fa524601d19e65c93c4561024b8fadb6beac
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-13 14:47:56 +0530

    we add script for run our project

---
+++ 10_URLShortnerProject/urlshortner/index.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv/config'
+import { app } from "./app.js";
+import { connectDB } from "./src/db/connection.js";
+
+// first we connect db.
+connectDB()
+.then(() => {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on PORT:${PORT}`);
+  })
+})
+.catch((err) => {
+  console.log(`We can't start server because database connection failed due to this error : ${err}`);
+})
