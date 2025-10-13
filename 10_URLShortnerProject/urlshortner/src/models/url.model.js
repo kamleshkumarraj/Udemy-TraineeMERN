c7dd94152dab8c0a271ab8d80f6a98e080aa24ae
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-13 16:29:13 +0530

    we create url schema

---
+++ 10_URLShortnerProject/urlshortner/src/models/url.model.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+
+const urlSchema = new mongoose.Schema({
+  urlCode : {
+    type : String,
+    required : true,
+    unique : true,
+    min : [3, "First name must be at least 3 characters long"],
+    max : [50, "First name must be at most 50 characters long"],
+    trim : true
+  },
+  longUrl : {
+    type : String,
+    required : true,
+    min : [3, "First name must be at least 3 characters long"],
+    trim : true
+  },
+  visited : [{
+    visitedBy : {
+      type : mongoose.Schema.Types.ObjectId,
+      ref : 'users'
+    }
+  }],
+  shortUrl : {
+    type : String,
+    required : true,
+    min : [3, "First name must be at least 3 characters long"],
+    max : [50, "First name must be at most 50 characters long"],
+    trim : true
+  },
+  generatedBy : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'users'
+  }
+})
+
+export const Urls = mongoose.model("urls", urlSchema);
