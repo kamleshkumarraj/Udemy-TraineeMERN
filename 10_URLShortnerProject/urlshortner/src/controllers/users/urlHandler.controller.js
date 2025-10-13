c0569a9c216144b538c7370f517f616e41567d6f
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-13 16:56:18 +0530

    we write code for generate short url

---
+++ 10_URLShortnerProject/urlshortner/src/controllers/users/urlHandler.controller.js
@@ -0,0 +1,31 @@
+import { ErrorHandler } from "../../errors/apiError.error.js";
+import { asyncErrorHandler } from "../../errors/asyncErrorHanlder.error.js";
+import shortID from 'short-id'
+import { Urls } from "../../models/url.model.js";
+
+export const minimizeURL = asyncErrorHandler(async (req, res, next) => {
+  const {url} = req.body;
+  if(!url) return next(new ErrorHandler("URL is required !", 400));
+
+  // if url is present then we sort the url.
+  const shortId = shortID();
+  const protocol = req.protocol;
+  const host = req.get('host');
+  const shortUrl = `${protocol}://${host}/${shortId}`
+
+  // now we save url in db.
+  const dbData = {
+    urlCode : shortId,
+    longUrl : url,
+    shortUrl,
+    generatedBy : req?.user?._id,
+    visited : [],
+  }
+
+  const urlData = await Urls.create(dbData);
+  res.status(200).json({
+    success : true,
+    message : "URL created successfully !",
+    data : urlData
+  })
+})
