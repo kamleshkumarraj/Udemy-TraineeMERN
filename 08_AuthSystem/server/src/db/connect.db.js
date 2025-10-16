7636391ba6e1fa7879452ddcc6543d85685d77d5
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-16 22:14:59 +0530

    we write code for connect db

---
+++ 08_AuthSystem/server/src/db/connect.db.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+
+
+export const connectDB = async () => {
+  const connect = await mongoose.connect(process.env.MONGO_URL);
+  console.log(`Database connected on host:${connect.connection.host} and port:${connect.connection.port}`)
+}
