cefed4eecc11b4101f6d0993a024275443334123
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-16 23:06:15 +0530

    we apply prettier format for formatting coding

--- 08_AuthSystem/server/src/app.js
+++ 08_AuthSystem/server/src/app.js
@@ -5,44 +5,50 @@ import cookieParser from 'cookie-parser';
 export const app = express();
 
 // now we write middleware that required for server setup.
-app.use(cors({
-  methods : ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  origin : ["http://localhost:5173"],
-  credentials : true,
-}))
+app.use(
+  cors({
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  }),
+);
 
 // middleware to parse body data.
-app.use(express.json({
-  limit : '50mb'
-}))
+app.use(
+  express.json({
+    limit: '50mb',
+  }),
+);
 
 // middleware for parsing cookies.
-app.use(cookieParser())
+app.use(cookieParser());
 
 // middleware for handling url.
-app.use(express.urlencoded({
-  extended : true,
-  limit : '5mb'
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: '5mb',
+  }),
+);
 
 // now we handle uncaught errors.
-process.on("uncaughtException", (err) => {
+process.on('uncaughtException', err => {
   console.log(err);
   process.exit(1);
 });
 
 // now we handle unhandled rejection.
-process.on("unhandledRejection", (err) => {
+process.on('unhandledRejection', err => {
   console.log(err);
   process.exit(1);
 });
 
 // middleware for error handling.
 app.use((err, req, res, next) => {
-  const message = err.message || "Interval server error !";
+  const message = err.message || 'Interval server error !';
   const statusCode = err.statusCode || 500;
   res.status(statusCode || 500).json({
-    success : false,
-    message : message
-  })
-})
+    success: false,
+    message: message,
+  });
+});
--- 08_AuthSystem/server/src/controllers/auth.controller.js
+++ 08_AuthSystem/server/src/controllers/auth.controller.js
@@ -1,50 +1,49 @@
 // controller for register student or faculty.
 export const register = asyncErrorHandler(async (req, res, next) => {
-
-  const {firstName, lastName, email, username, password, role, bio} = req.body;
-
+  const { firstName, lastName, email, username, password, role, bio } =
+    req.body;
 
   // first we check if user is already registered or not.
-  const existingUser = await Users.findOne({$or: [{ email }, { username }] });
+  const existingUser = await Users.findOne({ $or: [{ email }, { username }] });
 
-  if (existingUser) return next(new ErrorHandler("User already registered !", 400));
+  if (existingUser)
+    return next(new ErrorHandler('User already registered !', 400));
 
-   // now we create user.
+  // now we create user.
   await Users.create({
     firstName,
     lastName,
     email,
     username,
     password,
-    role : role || "user",
-    bio
-  })
+    role: role || 'user',
+    bio,
+  });
 
   res.status(200).json({
     success: true,
     message: `${role} registered successfully !`,
   });
-
 });
 
 // now we write controller for login student.
 export const login = asyncErrorHandler(async (req, res, next) => {
-  const { username, email, password, role="user" } = req.body;
+  const { username, email, password, role = 'user' } = req.body;
   // first we check student is registered or not.
   const user = await Users.findOne({
     $or: [{ email: email }, { username: username }],
-  }).select("+password");
+  }).select('+password');
 
-  if(user?.role !== role) return next(new ErrorHandler("Invalid credentials !", 400));
-
+  if (user?.role !== role)
+    return next(new ErrorHandler('Invalid credentials !', 400));
 
   // console.log(student);
 
-  if (!user) return next(new ErrorHandler("Invalid credentials !", 400));
+  if (!user) return next(new ErrorHandler('Invalid credentials !', 400));
 
   // if student is registered the we compare the password.
   if (!(await user.comparePassword(password)))
-    return next(new ErrorHandler("Invalid credentials !", 400));
+    return next(new ErrorHandler('Invalid credentials !', 400));
 
   // if password is correct then we login the student.
   loginWithJWT(user, res);
@@ -53,28 +52,28 @@ export const login = asyncErrorHandler(async (req, res, next) => {
 // now we write controller for logout student.
 export const logout = asyncErrorHandler(async (req, res, next) => {
   req.user = undefined;
-  res.clearCookie("token").status(200).json({
+  res.clearCookie('token').status(200).json({
     success: true,
-    message: "User logged out successfully.",
+    message: 'User logged out successfully.',
   });
 });
 
 // now we write controller for updating the profile.
 
 export const directLogin = asyncErrorHandler(async (req, res, next) => {
-  const user = await Users.findById(req.user,{role : 1, _id : 0}).lean();
+  const user = await Users.findById(req.user, { role: 1, _id: 0 }).lean();
   res.status(200).json({
     success: true,
-    message: "User logged in successfully.",
+    message: 'User logged in successfully.',
     data: user,
   });
-})
+});
 
 export const getProfile = asyncErrorHandler(async (req, res, next) => {
   const user = await Users.findById(req.user._id);
   res.status(200).json({
     success: true,
-    message: "User profile fetched successfully.",
+    message: 'User profile fetched successfully.',
     data: user,
   });
-})
+});
