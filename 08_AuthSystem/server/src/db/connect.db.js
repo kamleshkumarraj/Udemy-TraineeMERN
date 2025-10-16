8b3de4e99cf7c432295b19da62dd973bc2a8989b
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-16 23:36:09 +0530

    we fix bug

--- 08_AuthSystem/server/src/db/connect.db.js
+++ 08_AuthSystem/server/src/db/connect.db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-  const connect = await mongoose.connect(process.env.MONGO_URL);
+  const connect = await mongoose.connect(process.env.MONGODB_URL);
   console.log(
     `Database connected on host:${connect.connection.host} and port:${connect.connection.port}`,
   );
