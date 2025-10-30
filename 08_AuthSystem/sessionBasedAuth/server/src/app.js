6818c050268bed07063f12d2e24c77441f54e8c6
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-30 15:34:55 +0530

    we setup app folder for creating server

---
+++ 08_AuthSystem/sessionBasedAuth/server/src/app.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+export const app = express();
+
+app.use(express.json({
+  limit : '50mb'
+}))
+
+app.use(cors({
+  origin : ['http://localhost:5173'],
+  methods : ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  credentials : true
+}))
+
+app.use(cookieParser())
+
+// app.use(express.urlencoded({ extended: true }));
+
+// now we write code for handling unhandled rejection.
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection:", err);
+  // Application specific logging, throwing an error, or other logic here
+});
+
+// now we write code for handling uncaught exception.
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception:", err);
+  // Application specific logging, throwing an error, or other logic here
+});
+
+// global middleware for error handling.
+app.use((err, req, res, next) => {
+  // console.log(err.stack)
+  console.log("Hello")
+  const error = err?.message || "Something went wrong!";
+  const statusCode = err?.statusCode || 500;
+  res.status(statusCode).json({
+    success: false,
+    message: error,
+    statusCode: statusCode,
+  });
+});
