783e47c3831101e1760b7682153d882e28d89666
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-30 19:35:23 +0530

    we create models for centeralize user

---
+++ 08_AuthSystem/sessionBasedAuth/server/src/models/users.model.js
@@ -0,0 +1,66 @@
+// create user model for central user management.
+
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      pattern: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Email is invalid',
+      ],
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      min: [3, 'First name must be at least 3 characters long'],
+      max: [50, 'First name must be at most 50 characters long'],
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: [8, 'Password must be at least 8 characters long'],
+      max: [50, 'Password must be at most 50 characters long'],
+      trim: true,
+      pattern: [
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        'Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character.',
+      ],
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+    profile: {
+      type: mongoose.Schema.Types.ObjectId,
+      refPath: 'role',
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    avatar: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Users = mongoose.model('User', userSchema);
