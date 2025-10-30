e6f3bc050ce8b0b41dbb627780e57a205212d67c
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-30 18:48:35 +0530

    we write code for cloudinary configuration

--- 08_AuthSystem/sessionBasedAuth/server/src/constant.js
+++ 08_AuthSystem/sessionBasedAuth/server/src/constant.js
@@ -1,2 +1,8 @@
+// system related constant variable.
 export const PORT = process.env.PORT || 4000;
 export const MONGODB_URL = process.env.MONGODB_URL;
+
+// cloudinary related constant variable.
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
