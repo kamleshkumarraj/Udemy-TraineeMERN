ee5e5dae7129e5431c04329af6bb72c0e32e2c1c
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-10-30 19:03:58 +0530

    we write validators for register the user

---
+++ 08_AuthSystem/sessionBasedAuth/server/src/utils/multer.utils.js
@@ -0,0 +1,21 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'images');
+  },
+  filename: function (req, file, cb) {
+    const filePath = Date.now() + '-' + file.originalname;
+    cb(null, filePath);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+export const upload = multer({ storage: storage, fileFilter });
---
+++ 08_AuthSystem/sessionBasedAuth/server/src/validators/register.validator.js
@@ -0,0 +1,44 @@
+import { body } from 'express-validator';
+
+export const registerValidator = [
+  body('fullName')
+    .not()
+    .trim()
+    .isEmpty({ ignore_whitespace: true })
+    .withMessage('Name is required')
+    .isLength({ min: 3, max: 50 })
+    .withMessage('Name must be at least 3 characters long'),
+
+  body('email')
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Email is invalid'),
+
+  body('password')
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage('Password is required')
+    .isLength({ min: 8, max: 50 })
+    .withMessage('Password must be at least 6 characters long'),
+
+  body('confirmPassword')
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage('Password is required')
+    .isLength({ min: 8, max: 50 })
+    .withMessage('Password must be at least 6 characters long'),
+];
+
+export const validateRegister = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = errors.array().map((error) => error.msg).join(",");
+    return res.status(400).json({ errors: error});
+  }
+  next();
+};
