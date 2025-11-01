cec742ce8207d9ade3e2d6e773a4322d3fb96039
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-11-01 13:43:11 +0530

    we completely handle session

--- 08_AuthSystem/sessionBasedAuth/server/src/controllers/session.controller.js
+++ 08_AuthSystem/sessionBasedAuth/server/src/controllers/session.controller.js
@@ -2,26 +2,29 @@ import { asyncErrorHandler } from '../errors/asyncErrorHandler.js';
 import { Session } from '../models/session.models.js';
 
 export const createSession = asyncErrorHandler(async (req, res, next) => {
-  const {_sid} = req.signedCookies;
+  const { _sid } = req.signedCookies;
 
   if (_sid) {
     const session = await Session.findById(_sid);
-    if(session.expiresAt > Math.round(Date.now() / 1000)) {
-      return res.status(200).json({
-        success: true,
-        message: 'Session already exists',
-        session,
-      });
+    if (session) {
+      if (new Date(session.expiresAt).valueOf() > Date.now()) {
+        return res.status(200).json({
+          success: true,
+          message: 'Session already exists',
+        });
+      }else{
+        await Session.findByIdAndDelete(_sid);
+      }
     }
   }
 
   const session = await Session.create({
-    expiresAt: Math.round(Date.now() / 1000) + 60 * 60 * 24 * 10,
+    expiresAt: Date.now() + 1000 * 60 * 60 * 24 * 10,
   });
   const options = {
     httpOnly: true,
     sameSite: 'None',
-    secure : true,
+    secure: true,
     maxAge: 1000 * 60 * 60 * 24 * 10,
     signed: true,
   };
