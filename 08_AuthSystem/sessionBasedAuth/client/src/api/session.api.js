e692056152fd19b11b4e5f107e8176c97113e0f7
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-11-01 11:54:25 +0530

    we write endpoints for create session

---
+++ 08_AuthSystem/sessionBasedAuth/client/src/api/session.api.js
@@ -0,0 +1,15 @@
+import { baseApi } from "./api";
+
+const sessionApi = baseApi.injectEndpoints({
+  endpoints : (builder) => ({
+    createSession : builder.mutation({
+      query : () => ({
+        url : '/session/create',
+        method : 'POST',
+        credentials : 'include'
+      })
+    })
+  })
+})
+
+export const {useCreateSessionMutation} = sessionApi
