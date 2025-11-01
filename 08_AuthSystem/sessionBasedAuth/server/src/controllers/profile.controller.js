21122f8264c328090bcf3df0be88b0b6bda4e385
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-11-01 15:29:17 +0530

    we write endpoints for get user profile

---
+++ 08_AuthSystem/sessionBasedAuth/server/src/controllers/profile.controller.js
@@ -0,0 +1,27 @@
+import { asyncErrorHandler } from "../errors/asyncErrorHandler.js";
+import { ErrorHandler } from "../errors/errorHandler.js";
+import { Session } from "../models/session.models.js";
+import { Users } from "../models/users.model.js";
+
+
+export const getMyProfile = asyncErrorHandler(async (req, res, next ) => {
+  const {_sid} = req.signedCookies;
+
+  if(!_sid){
+    return next(new ErrorHandler("please login to access this resource !",402));
+  }
+
+  const userSession = await Session.findOne({_id : _sid});
+
+  if(!userSession?.userId){
+    return next(new ErrorHandler("please login to access this resource !",402));
+  }
+
+  const userProfile = Users.findById(userSession.userId);
+
+  res.status(200).json({
+    success : true,
+    message : "user profile",
+    data : userProfile
+  })
+})
