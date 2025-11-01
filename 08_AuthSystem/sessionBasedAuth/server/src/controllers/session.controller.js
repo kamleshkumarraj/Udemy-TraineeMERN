296609c4acab9ef1a7731228097a4b52e4bb50f9
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-11-01 12:21:50 +0530

    we handle already exists session

--- 08_AuthSystem/sessionBasedAuth/server/src/controllers/session.controller.js
+++ 08_AuthSystem/sessionBasedAuth/server/src/controllers/session.controller.js
@@ -2,12 +2,26 @@ import { asyncErrorHandler } from '../errors/asyncErrorHandler.js';
 import { Session } from '../models/session.models.js';
 
 export const createSession = asyncErrorHandler(async (req, res, next) => {
+  const {_sid} = req.signedCookies;
+
+  if (_sid) {
+    const session = await Session.findById(_sid);
+    if(session.expiresAt > Math.round(Date.now() / 1000)) {
+      return res.status(200).json({
+        success: true,
+        message: 'Session already exists',
+        session,
+      });
+    }
+  }
+
   const session = await Session.create({
     expiresAt: Math.round(Date.now() / 1000) + 60 * 60 * 24 * 10,
   });
   const options = {
     httpOnly: true,
-    sameSite: None,
+    sameSite: 'None',
+    secure : true,
     maxAge: 1000 * 60 * 60 * 24 * 10,
     signed: true,
   };
--- 08_AuthSystem/sessionBasedAuth/server/src/models/session.models.js
+++ 08_AuthSystem/sessionBasedAuth/server/src/models/session.models.js
@@ -4,7 +4,6 @@ const sessionSchema = new mongoose.Schema({
   userId : {
     type : mongoose.Schema.Types.ObjectId,
     ref : 'users',
-    required : true,
     default : null
   },
   expiresAt : {
